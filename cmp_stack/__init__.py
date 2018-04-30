2e1ff1898f5e72bcd1bedad265f431e9ad564eeb
Georgia Stuart <georgia.stuart@gmail.com>
2018-04-30 14:25:31 -0500

    Misadventures in interpolating

--- cmp_stack/__init__.py
+++ cmp_stack/__init__.py
@@ -2,3 +2,4 @@ from cmp_stack.radon_transform import RadonTransform
 from cmp_stack.nmo import NormalMoveOut
 from cmp_stack.mute_gain import Mute, gain
 from cmp_stack.cmp_stack import cmp_stack, stack
+from cmp_stack.interpolate_missing_data import interpolate_missing_data
---
+++ cmp_stack/interpolate_missing_data.py
@@ -0,0 +1,32 @@
+import numpy as np
+from scipy.interpolate import interp2d
+
+
+# Didn't work well!
+def interpolate_missing_data(cmp_gather):
+    num_time_steps = cmp_gather.shape[0]
+    num_receivers = cmp_gather.shape[1]
+
+    times = np.array(range(num_time_steps))
+    offsets = np.array(range(num_receivers))
+    zero_receiver_indices = []
+
+    for i in range(num_receivers):
+        if not len(cmp_gather[cmp_gather[:, i] != 0, i]):
+            zero_receiver_indices.append(i)
+
+    print(zero_receiver_indices)
+
+    if len(zero_receiver_indices):
+        print(np.delete(offsets, zero_receiver_indices))
+        interp_func = interp2d(np.delete(offsets, zero_receiver_indices), times,
+                               np.delete(cmp_gather, zero_receiver_indices, axis=1), kind='cubic')
+        cmp_gather[:, zero_receiver_indices] = interp_func(offsets[zero_receiver_indices], times)
+    # for i in range(num_time_steps):
+    #     try:
+    #         interp_func = interp1d(offsets[cmp_gather[i, :] != 0], cmp_gather[i, cmp_gather[i, :] != 0], kind='cubic')
+    #         cmp_gather[i, cmp_gather[i, :] == 0] = interp_func(offsets[cmp_gather[i, :] == 0])
+    #     except ValueError:
+    #         pass
+
+
